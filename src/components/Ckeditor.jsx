6afa5073906d057a3f538ee7b3e49db8c55dceb9
Chea Phanith <cheaphanith359@gmail.com>
2022-07-11 08:31:32 +0700

    added

--- src/components/Ckeditor.jsx
+++ src/components/Ckeditor.jsx
@@ -1,34 +1,60 @@
-import React, { Component } from "react";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CKEditor
-          editor={ClassicEditor}
-          data="<h1 >ReacJs for beginer</h1>"
-          onReady={(editor) => {
-            // You can store the "editor" and use when it is needed.
-            console.log("Editor is ready to use!", editor);
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
-          }}
-          onBlur={(event, editor) => {
-            console.log("Blur.", editor);
-          }}
-          onFocus={(event, editor) => {
-            console.log("Focus.", editor);
-          }}
-          
-        />
-      </div>
-    );
-  }
+export default function Ckeditor() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+      console.log(editorRef.current);
+    }
+  };
+
+  return (
+    <>
+      <Editor
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: true,
+          convert_fonts_to_spans: true,
+          font_size_style_values: "8pt,10pt,12pt,14pt,18pt,24pt,36pt",
+          plugins: [
+            "a11ychecker",
+            "advlist",
+            "advcode",
+            "advtable",
+            "autolink",
+            "checklist",
+            "export",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "powerpaste",
+            "fullscreen",
+            "formatpainter",
+            "insertdatetime",
+            "media",
+            "table",
+            "help",
+            "wordcount",
+            "save",
+          ],
+          toolbar:
+            "undo redo save| casechange blocks | fontselect fontsizeselect bold italic backcolor |" +
+            "alignleft aligncenter alignright alignjustify | " +
+            "bullist numlist checklist outdent indent | removeformat | a11ycheck code table help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </>
+  );
 }
-
-
-export default App;
