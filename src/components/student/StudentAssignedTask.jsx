7935eb68bee475a44da1bbc4e42acadf1f21c3fd
kimgech1 <ployjaruwan83@gmail.com>
2022-07-13 14:09:15 +0700

    stu assigntask

--- src/components/student/StudentAssignedTask.jsx
+++ src/components/student/StudentAssignedTask.jsx
@@ -6,55 +6,60 @@ import { fetchAllCommentByClassMaterial } from "../../service/commentService";
 
 export default function StudentAssignedTask({ assignedTask }) {
   const [comment, setComment] = useState([]);
-  // const dataTask = assignedTask;
+  const dataTask = assignedTask;
   function onHandleComment(classMaterialId) {
     fetchAllCommentByClassMaterial(classMaterialId).then((r) => {
-      console.log('====================================');
+      console.log("====================================");
       console.log(r.data);
-      console.log('====================================');
+      console.log("====================================");
       setComment(r.data);
     });
   }
   return (
     <div className="flex flex-wrap w-full max-w-sm p-4 border rounded-md shadow-xl mr-7 mb-7 h-52 border-bordergray">
-     <div className="w-full">
-     <div className="flex text-2xl text-black cursor-default">
+      <div className="w-full">
+        <div className="flex text-2xl text-black cursor-default">
           <MdOutlineAssignment />
-          <span className="ml-2 text-lg font-medium">{assignedTask?.title}</span>
+          <span className="ml-2 text-lg font-medium">{dataTask?.title}</span>
         </div>
-      <div className="mt-2 text-base font-medium">
-        <p className="max-w-[22rem] truncate text-black">
-          {assignedTask?.description}
-        </p>
-      </div>
-      <div className="text-xs font-medium">
-        <div className="mb-2">
-          Point: <span>{assignedTask?.score}</span>
+        <div className="mt-2 text-base font-medium">
+          <p className="max-w-[22rem] truncate text-black">
+            {dataTask?.description}
+          </p>
         </div>
-        <div className="mt-1 text-xs font-regular text-myred ">
-          <p> <span>Due: </span> <span>{assignedTask?.deadline}</span></p>
-        </div>
-      </div>
-      <div className="flex mt-auto">
-        <NavLink
-          to="/work-submit"
-          type="link"
-          className="px-4 py-1 text-base text-center text-white shadow-md bg-mygreen font-regular rounded-xl h-max"
-        >
-          View
-        </NavLink>
-        <div
-          className="mt-2 ml-40 text-sm underline font-regular dropdown dropdown-right"
-          onClick={()=>onHandleComment(assignedTask.material_id)}
-        >
-          <label tabindex="0" className="underline dropdown dropdown-right hover:cursor-pointer">
-            <span>{assignedTask.total_comment}</span> comments
-          </label>
-          <div tabindex="0" className="mt-2 dropdown-content rounded-box">
-            <StudentComment comment={comment} />
+        <div className="text-xs font-medium">
+          <div className="mb-2">
+            Point: <span>{dataTask?.score}</span>
+          </div>
+          <div className="mt-1 text-xs font-regular text-myred ">
+            <p>
+              <span>Due: </span> <span>{dataTask?.deadline}</span>
+            </p>
           </div>
         </div>
-        {/* <div className="mt-2 ml-40 text-sm underline font-regular dropdown dropdown-right">
+        <div className="flex mt-auto">
+          <NavLink
+            to="/work-submit"
+            type="link"
+            className="px-4 py-1 text-base text-center text-white shadow-md bg-mygreen font-regular rounded-xl h-max"
+          >
+            View
+          </NavLink>
+          <div
+            className="mt-2 ml-40 text-sm underline font-regular dropdown dropdown-right"
+            onClick={() => onHandleComment(assignedTask.material_id)}
+          >
+            <label
+              tabindex="0"
+              className="underline dropdown dropdown-right hover:cursor-pointer"
+            >
+              <span>{dataTask.total_comment}</span> comments
+            </label>
+            <div tabindex="0" className="mt-2 dropdown-content rounded-box">
+              <StudentComment comment={comment} />
+            </div>
+          </div>
+          {/* <div className="mt-2 ml-40 text-sm underline font-regular dropdown dropdown-right">
           <label tabindex="0" className="underline dropdown dropdown-right">
             <span>2</span> comments
           </label>
@@ -62,8 +67,8 @@ export default function StudentAssignedTask({ assignedTask }) {
             <StudentComment comment={comment}/>
           </div>
         </div> */}
+        </div>
       </div>
-     </div>
     </div>
   );
 }
--- src/components/student/StudentComment.jsx
+++ src/components/student/StudentComment.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 
-export default function StudentComment() {
+export default function StudentComment({comment}) {
+  console.log('====================================');
+  console.log(comment);
+  console.log('====================================');
   return (
     <div className="z-50 p-3 bg-white border shadow-md border-mygray w-max card">
-      <div className="flex mb-2">
+     {comment?.map((data) => (
+          <ul className="flex mb-2">
+            <li className="flex">
+            <img
+              className="flex object-cover rounded-full h-9 w-9 jsutify-left"
+              src={
+                data.img !== null
+                  ? data.img
+                  : "https://www.cmcaindia.org/wp-content/uploads/2015/11/default-profile-picture-gmail-2.png"
+              }
+              alt="stupf"
+            />
+
+            <div className="ml-2">
+              <div className="flex">
+                <p className="font-medium">{data.name}</p>
+                <p className="mt-1 ml-3 text-xs">{data.comment_date.toString().substring(0, 16)}</p>
+              </div>
+              <label className="block text-sm">{data.comment}</label>
+            </div></li>
+          </ul>
+       
+      ))}
+      {/* <div className="flex mb-2">
         <img
           className="flex object-cover rounded-full h-9 w-9 jsutify-left"
           src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
           alt=""
         />
-
         <div className="ml-2">
           <div className="flex">
             <p className="font-medium"> Dayan</p>
@@ -33,7 +58,7 @@ export default function StudentComment() {
             Noted
           </label>
         </div>
-      </div>
+      </div> */}
       <div className="flex">
         <img
           className="flex object-cover rounded-full h-9 w-9 jsutify-left"
