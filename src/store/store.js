bcaede197003179f386a96196f1216e962ba8af7
Chea Phanith <cheaphanith359@gmail.com>
2022-07-13 09:44:36 +0700

    merge s

--- src/store/store.js
+++ src/store/store.js
@@ -4,41 +4,44 @@ import scheduleStudentSlice from "../slices/schedule/scheduleStudentSlice";
 import userSlice from "../slices/users/userSlice";
 import { applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-
-
+import userReducer from "../slices/users/userSlice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const { configureStore } = require("@reduxjs/toolkit");
-import {persistStore,persistReducer,  FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER} from 'redux-persist'
-import userReducer from '../slices/users/userSlice'
-import storage from 'redux-persist/lib/storage'
-
-
-const { configureStore } = require("@reduxjs/toolkit");
-
 const persistConfig = {
-  key: 'root',
-  
-  storage,
-  blacklist:['user']
-}
+  key: "root",
 
-const persistedReducer = persistReducer(persistConfig, userReducer)
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
     //class material
-    persistedReducer:persistedReducer,
+    persistedReducer: persistedReducer,
     classes: classesSlice,
     scheduleTeacher: scheduleTeacherSlice,
     scheduleStudent: scheduleStudentSlice,
     user: userSlice,
   },
   middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware({
-    serializableCheck: {
-        ignoredActions:[FLUSH,PAUSE,PERSIST]
-      }
-    })
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, PAUSE, PERSIST],
+      },
+    }),
 });
 
 /*
