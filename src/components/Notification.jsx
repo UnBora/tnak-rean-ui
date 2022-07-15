9bd00954fbde54d1ca43676cede13a64b12416f7
Chea Phanith <cheaphanith359@gmail.com>
2022-07-16 04:37:16 +0700

    added notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { fetchNotification } from "../service/notification";
 
 export default function ProfileCard() {
   const [notification, setNotification] = useState([]);
+  const users = useSelector((state) => state?.persistedReducer?.value);
   useEffect(() => {
-    fetchNotification(1).then((r) => {
+    fetchNotification(users.id).then((r) => {
       setNotification(r.data);
       console.log("notificaiton", r);
     });
@@ -15,30 +17,31 @@ export default function ProfileCard() {
         <p className="-mt-4 text-xl font-medium text-mygreen">Notification</p>
         <p className="myhr"></p>
         {notification?.map((index) => {
-        return (
-          <div>
-            <div className="flex items-center space-x-2 cursor-pointer hover:bg-gray-200 hover:rounded ">
-            <div className="ml-2 avatar">
-              <div className="w-10 h-10 rounded-full mask mask-squircle">
-              <img src="https://api.lorem.space/image/face?hash=53273" 
-              //  src={
-              //   index.sender_img !== null
-              //     ? index.sender_img
-              //     : "https://www.cmcaindia.org/wp-content/uploads/2015/11/default-profile-picture-gmail-2.png"
-              // }
-              // alt="pf"
-              alt="profile" 
-              />
+          return (
+            <div>
+              <div className="flex items-center space-x-2 cursor-pointer hover:bg-gray-200 hover:rounded ">
+                <div className="ml-2 avatar">
+                  <div className="w-10 h-10 rounded-full mask mask-squircle">
+                    <img
+                      src="https://api.lorem.space/image/face?hash=53273"
+                      //  src={
+                      //   index.sender_img !== null
+                      //     ? index.sender_img
+                      //     : "https://www.cmcaindia.org/wp-content/uploads/2015/11/default-profile-picture-gmail-2.png"
+                      // }
+                      // alt="pf"
+                      alt="profile"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <span className="font-bold">{index.sender_name}</span>
+                  {/* <span>{index.sender_name}to join Phnom Penh classroom</span> */}
+                </div>
               </div>
             </div>
-            <div >
-              <span className="font-bold">{index.sender_name}</span>
-              {/* <span>{index.sender_name}to join Phnom Penh classroom</span> */}
-            </div>
-          </div>
-          </div>
-        );
-      })}
+          );
+        })}
       </div>
     </div>
   );
--- src/service/notification.js
+++ src/service/notification.js
@@ -1,18 +1,15 @@
 import { api } from "../utils/api";
 
 export const fetchNotification = async (userId) => {
-    try {
-      let response = await api.get(
-        "user/get-notification-by-userId",
-        {
-          params: {
-            
-            userId:userId
-          },
-        }
-      );
-      return response.data;
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
+  try {
+    let response = await api.get("notification/get", {
+      params: {
+        userId: userId,
+      },
+    });
+    console.log('test...... ',response.data);
+    return response.data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
